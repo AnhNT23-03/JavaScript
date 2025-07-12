fe76b8a8d442df0e0f897392b721d6742483069c
TuanNt199 <mantraruma@outlook.com>
2025-07-12 15:47:34 +0700

    Toán tử gán trong JavaScript

---
+++ 9. Assignment operator in JavaScript/Toán tử gán trong JavaScript.md
@@ -0,0 +1,7 @@
+# nội dung bài học trên youtube
+Bài 9. Toán tử gán trong JavaScript Hướng dẫn từ cơ bản đến nâng cao - Series Tự học JS từ A đến Z
+    - link bài học : https://www.youtube.com/watch?v=Tgs1_HCP7rQ&list=PLPt6-BtUI22pYwpfmkP4EuJkf6GRe63KU&index=13
+
+1. Nội dung bài học 9:
+- Toán tử gán ![alt text](./assets/toantugan.png) https://youtu.be/Tgs1_HCP7rQ?list=PLPt6-BtUI22pYwpfmkP4EuJkf6GRe63KU&t=20
+- Kết quả demo : ![alt text](./assets/demotoantu.png)
---
+++ 9. Assignment operator in JavaScript/demo/main.js
@@ -0,0 +1,36 @@
+// 09. Toán tử gán
+// 1. Toán Tử Gán (=) Dùng để gán giá trị cho biến.
+let x = 10; // Biến x được gán giá trị 10
+let y = 5; // Biến y được gán giá trị 5
+let z = x + y; // Biến z được gán giá trị là tổng của x và y
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+// Gán +=, -= , *= , /= , %=
+console.log("Kết quả gán +=, -= , *= , /= , %= ");
+let m= 10;
+m += 5; // Tương đương với m= m + 5;
+console.log(m); // In ra 15
+
+let n = 8;
+n -= 3; // Tương đương với n = n - 3;
+console.log(n); // In ra 5
+
+let p = 6;
+p *= 2; // Tương đương với p = p * 2;
+console.log(p); // In ra 12
+
+let q = 9;
+q /= 3; // Tương đương với q = q / 3;
+console.log(q); // In ra 3
+
+let r = 4;
+r %= 3; // Tương đương với r = r % 3;
+console.log(r); // In ra 1
+
+
+
+
+// giải bài tập js
