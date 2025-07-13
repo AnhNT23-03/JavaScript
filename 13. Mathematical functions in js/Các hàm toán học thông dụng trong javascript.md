848e01af712f1c86b66ea29104f87aaad3e199b9
TuanNt199 <mantraruma@outlook.com>
2025-07-13 13:39:18 +0700

    Các hàm toán học thông dụng trong javascript

---
+++ 13. Mathematical functions in js/Các hàm toán học thông dụng trong javascript.md
@@ -0,0 +1,10 @@
+# nội dung bài học trên youtube
+    - Bài 13. Các hàm toán học thông dụng trong javascript - Học JavaScript căn bản cho người mới
+    - link bài học: https://www.youtube.com/watch?v=L7HlhpQwEsk&list=PLPt6-BtUI22pYwpfmkP4EuJkf6GRe63KU&index=21
+
+1. Nội dung bài học:
+- Các hàm toán học thông dụng 
+![phần 1](./assets/image-1.png), 
+![phần 2](./assets/image-2.png),
+![phần 3](./assets/image-3.png),
+![phần 4](./assets/image-4.png)
---
+++ 13. Mathematical functions in js/demo/main.js
@@ -0,0 +1,35 @@
+// 13. các hàm toán học thông dụng
+// 1. Hàm Math.sqrt()
+let a = Math.sqrt(4);
+console.log(a);
+
+// 2. Hàm Math.pow(base, exponent)'
+let b = Math.pow(2, 3);
+console.log(b);
+
+// 3. Hàm Math. abs()'
+let c = Math.abs(10);
+console.log(c);
+
+// 4. Hàm Math.ceil() và `Math. floor()'
+let d = Math.floor(5.004);
+console.log(d);
+
+// 5. Hàm `Math. round(), từ 0.5 làm tròn thành 1
+let e = Math.round(7.499999);
+console.log(e);
+
+//6. làm tròn x chữ số phần đơn vị
+let f = 3.14159;
+let fRounded = Number(f.toFixed(2));
+console.log(fRounded) ;
+console.log(typeof fRounded);
+// cộng thử
+let g = fRounded + 5;
+console.log(g);
+
+// 7. Hàm Math.min() và Math.max()
+// let h = Math.max(1, 2,)
+let h = Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9, 100)
+console.log(h);
+
